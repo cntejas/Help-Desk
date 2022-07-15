7c21c975da39086505f6acab99f654d25bf9c466
C N Tejas <cntejasc@gmail.com>
2022-07-15 12:35:17 +0530

    prepare for deploy

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const express =  require('express')
+const colors =  require('colors')
+const dotenv =  require('dotenv').config()
+const { errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5000
+
+//connect to database
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
+
+
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+//serve frontend
+
+if(process.env.NODE_ENV ===  'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req,res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else {
+    app.get('/', (req, res) => {
+        res.send('Welcome to support desk API')
+        //res.status(200).json({message:'Welcome to support desk API'})
+    })
+}
+
+
+
+app.use(errorHandler)
+
+app.listen( PORT, () => console.log(`Server started on PORT ${PORT}`))
